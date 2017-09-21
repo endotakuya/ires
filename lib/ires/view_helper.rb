b83609581b6b018fd6c1da1c0adf924501e47dd8
enta0701 <endo.takuya.0701@gmail.com>
2017-09-21 12:02:10 +0900

    ファイル名をルートからのパスに変更する

--- lib/ires/view_helper.rb
+++ lib/ires/view_helper.rb
@@ -4,14 +4,16 @@ module Ires
     # Image resize
     # return [image_tag]
     def ires_tag(path:, width:, height:, mode:, **option)
+      full_path = image_full_path(path.to_s)
+
       # Reszie image 
       case mode
       when "resize"
-        @image = Ires::Service.resizeImage(path.to_s, width, height, image_dir)
+        @image = Ires::Service.resizeImage(full_path, width, height, image_dir)
       when "crop"
-        @image = Ires::Service.cropImage(path.to_s, width, height, image_dir)
+        @image = Ires::Service.cropImage(full_path, width, height, image_dir)
       when "resize_to_crop"
-        @image = Ires::Service.resizeToCropImage(path.to_s, width, height, image_dir)  
+        @image = Ires::Service.resizeToCropImage(full_path, width, height, image_dir)  
       end
 
       return nil if @image.nil?
@@ -25,6 +27,15 @@ module Ires
       # Reszie image directory
       # return [String]
       @image_dir ||= Pathname.new(Rails.root).join("public").to_s
+    end
+
+    def image_full_path(path)
+      root = Rails.root.to_s
+      if path.include?(root) || path.include?("http")
+        return path
+      else
+        return File.join(image_dir, path)
+      end
     end
 
   end
